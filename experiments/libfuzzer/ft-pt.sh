b58aafa075a431c6f708620671a4f136c8d689b7
Elwin Tamminga <e.tamminga@ultraware.nl>
2023-09-25 14:25:00 +0200

    Add fine-tuned pt experiment script

---
+++ experiments/libfuzzer/ft-pt.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ ! -d "$1" ]; then
+  echo "$1 does not exist."
+  exit 1
+fi
+if [ -z "$2" ]; then
+  echo "Need args"
+  exit 1
+fi
+args="${@:2}"
+
+for i in {1..4}; do
+  for conf in ../../configs/*; do
+    bash ./collect.sh "$1"/ft/$(basename $conf)_$i \
+      sh -c "cd source && python3 ../../../seedai.py -v -p ../../../bin/goparser -n 10 -d ../corpus -c ../$conf -pt ../../../pt_configs/go/openai_ft/gpt-3.5-turbo.json $args" || exit 1
+  done
+done
