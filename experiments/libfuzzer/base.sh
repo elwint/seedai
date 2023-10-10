dde50c2f97f8df4c78e4c5d08e1f320dcdb6d470
Elwin Tamminga <e.tamminga@ultraware.nl>
2023-10-10 15:51:50 +0200

    Update base experiment scripts

--- experiments/libfuzzer/base.sh
+++ experiments/libfuzzer/base.sh
@@ -4,7 +4,7 @@ for dir in source_*; do
   folder_name_without_prefix="${dir#source_}"
   results="./results/$folder_name_without_prefix"
 
-  for i in {1..14}; do
+  for i in {1..28}; do
     bash ./collect.sh "$results"/base/$i || exit 1
   done
 done
--- experiments/libfuzzer/base_corp.sh
+++ experiments/libfuzzer/base_corp.sh
@@ -4,9 +4,9 @@ for dir in source_*; do
   folder_name_without_prefix="${dir#source_}"
   results="./results/$folder_name_without_prefix"
 
-  for i in {1..14}; do
+  for i in {1..28}; do
     bash ./collect.sh "$results"/base_corp/$i \
-      sh -c "rm -r corpus && cp -r base_corp corpus" || exit 1
+      sh -c "rm -r corpus && cp -r $dir/base_corp corpus" || exit 1
   done
 done
 echo "Done ..."
